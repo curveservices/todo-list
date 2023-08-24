e284966bd06a631946e3f90bc4a55c31adbffa75
Paul <p.rossiter83@gmail.noreply.github.com>
2023-08-24 20:18:59 +0100

    Dark mode code added

--- src/initial-dom.js
+++ src/initial-dom.js
@@ -4,20 +4,27 @@ import { blankProject } from "./blank-project";
 export function initialdom () {
 
     //DOM for adding Todo
+
+    // Dark mode
     const content = document.querySelector('.content');
-    const addButton = document.createElement('div');
     
-    const btn = document.createElement('button');
-    btn.textContent = '+';
-    btn.classList = 'tab';
-    btn.setAttribute('id', 'new-todo')
+    const toggle = document.getElementById('toggle-dark');
+    const taskBtn = document.getElementById('toggle-dark');
+    const body = document.querySelector('body');
+    const header = document.querySelector('header');
 
-    const heading = document.createElement('h2');
-    heading.textContent = 'Add ToDo';
-
-    content.appendChild(addButton);
-    addButton.appendChild(btn);
-    addButton.appendChild(heading);
+    toggle.addEventListener('click', function() {
+        this.classList.toggle('bi-moon');
+        if (this.classList.toggle('bi-brightness-high-fill')) {
+            body.style.background = '#ffffff';
+            body.style.color = 'black';
+            body.style.transition = '0.1s';
+        } else {
+            body.style.background = '#344a67';
+            body.style.color = 'white';
+            body.style.transition = '0.1s';
+        }
+    });
 
     //DOM for the default project load
     const projectsInfoDiv = document.createElement('div');
